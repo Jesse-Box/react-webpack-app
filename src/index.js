1e6e10ed9ac50d84d383ea814529d306835059c6
Jesse Box <me@jessebox.net>
2024-11-15 12:04:10 +0100

    Bug: Forgot to import Sentry

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
+import * as Sentry from '@sentry/react';
 
 Sentry.init({
   dsn: 'https://e184d7ff627ea830234a804f59282bda@o925438.ingest.us.sentry.io/4508301866762240',
