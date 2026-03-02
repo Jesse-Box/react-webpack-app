975a0d604110975b6544d8d262fb3fdc489113fe
Jesse Box <me@jessebox.net>
2026-03-02 15:26:15 -0800

    Modified: dependency list for package.json

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
-import * as Sentry from '@sentry/react';
-
-Sentry.init({
-  dsn: 'https://e184d7ff627ea830234a804f59282bda@o925438.ingest.us.sentry.io/4508301866762240',
-  integrations: [],
-});
 
 function App() {
   return (
     <div>
       <h1>This is my React app!</h1>
-      <button
-        onClick={() => {
-          throw new Error('This is your first error!');
-        }}
-      >
-        Send Error
-      </button>
     </div>
   );
 }
